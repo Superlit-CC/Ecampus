ba32302ca4f4036eae04668a8bed8538047f604b
superlit <2762183147@qq.com>
2023-12-19 15:40:51 +0800

    update voucher create and query by shop id

--- src/main/java/cn/cc/ecampus/service/IVoucherService.java
+++ src/main/java/cn/cc/ecampus/service/IVoucherService.java
@@ -1,5 +1,6 @@
 package cn.cc.ecampus.service;
 
+import cn.cc.ecampus.dto.Result;
 import cn.cc.ecampus.entity.Voucher;
 import com.baomidou.mybatisplus.extension.service.IService;
 
@@ -8,4 +9,7 @@ import com.baomidou.mybatisplus.extension.service.IService;
  * @create 2023/12/14 11:01
  */
 public interface IVoucherService extends IService<Voucher> {
+    void addSeckillVoucher(Voucher voucher);
+
+    Result queryVoucherOfShop(Long shopId);
 }
--- src/main/java/cn/cc/ecampus/service/impl/VoucherServiceImpl.java
+++ src/main/java/cn/cc/ecampus/service/impl/VoucherServiceImpl.java
@@ -1,10 +1,15 @@
 package cn.cc.ecampus.service.impl;
 
+import cn.cc.ecampus.dto.Result;
+import cn.cc.ecampus.entity.SeckillVoucher;
 import cn.cc.ecampus.entity.Voucher;
 import cn.cc.ecampus.mapper.VoucherMapper;
+import cn.cc.ecampus.service.ISeckillVoucherService;
 import cn.cc.ecampus.service.IVoucherService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * @author superlit
@@ -12,4 +17,27 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class VoucherServiceImpl extends ServiceImpl<VoucherMapper, Voucher> implements IVoucherService {
+
+    @Resource
+    private ISeckillVoucherService seckillVoucherService;
+
+    @Override
+    @Transactional // 保证两个数据库表的一致性
+    public void addSeckillVoucher(Voucher voucher) {
+        // 保存优惠券
+        save(voucher);
+        // 保存秒杀信息
+        SeckillVoucher seckillVoucher = new SeckillVoucher();
+        seckillVoucher.setVoucherId(voucher.getId());
+        seckillVoucher.setStock(voucher.getStock());
+        seckillVoucher.setBeginTime(voucher.getBeginTime());
+        seckillVoucher.setEndTime(voucher.getEndTime());
+        seckillVoucherService.save(seckillVoucher);
+    }
+
+    @Override
+    public Result queryVoucherOfShop(Long shopId) {
+        // TODO 查询店铺的优惠券列表
+        return Result.fail("功能暂未实现！");
+    }
 }
